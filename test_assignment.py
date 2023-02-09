bd547e7e403e002affca45d0179971c9f6915fcc
Josette Content <jmc2422@columbia.edu>
2023-02-09 17:01:08 -0500

    added slot function

--- test_assignment.py
+++ test_assignment.py
@@ -63,8 +63,7 @@ def hole(svg, side, location, start_x, start_y, rec_length, rec_width):
                 dot = svg.write('<circle cx = "{}" cy = "{}" r = "{}" ' \
                     'stroke-width="2" stroke="black" fill="black"/>\n'.format(i * rec_width/3 + start_x, -screwhole_edge + start_y + rec_length, screwhole))
             return dot
-        else:
-            return "define side"
+
 
 def main():
     start_x = 50
@@ -100,12 +99,87 @@ def main():
         svg.write('<rect x="{}" y="{}" width="{}" height="{}" ' \
             'stroke-width="2" stroke="black" fill="none"/>\n'.format(start_x, start_y, rec_width, rec_length)) 
         
-               
-        #screw holes for base
-        hole(svg, "w", "f", start_x, start_y, rec_length, rec_width)
-        hole(svg, "w", "b", start_x, start_y, rec_length, rec_width)
-        hole(svg, "l", "r", start_x, start_y, rec_length, rec_width)
-        hole(svg, "l", "l", start_x, start_y, rec_length, rec_width)
+        #test screw slot
+        # svg.write('<polyline points = "{},{} {},{} {},{} {},{} {},{} {},{}, {},{} {},{} {},{} {},{} {},{} {},{}" ' \
+        #     'stroke-width="2" stroke="black" fill="none"/>\n'.format(slotx, sloty, slotx, sloty+.155*96,  
+        #         slotx-.05*96, sloty+.155*96, slotx-.05*96, sloty+.225*96, slotx, sloty+.225*96,
+        #         slotx, sloty+.38*96, slotx+.09*96, sloty+.38*96, slotx+.09*96, sloty+.225*96, 
+        #         slotx+.14*96, sloty+.225*96, slotx+.14*96, sloty+.155*96, slotx+.09*96, sloty+.155*96, 
+        #         slotx+.09*96, sloty))
+
+        def slot(svg, side, location, start_x, start_y, rec_length, rec_width):
+            if side == "l" and location == "l": #length and left                
+                for i in range(1, 3, 1):   
+                    slotx = start_x 
+                    sloty = i * rec_length/3 + start_y + .045*96       
+                    svg.write('<polyline points = "{},{} {},{} {},{} {},{} {},{} {},{}, {},{} {},{} {},{} {},{} {},{} {},{}" ' \
+                        'stroke-width="2" stroke="black" fill="none" transform="rotate(270,{},{})"/>\n'.format(slotx, sloty, slotx, sloty+.155*96,  
+                        slotx-.05*96, sloty+.155*96, slotx-.05*96, sloty+.225*96, slotx, sloty+.225*96,
+                        slotx, sloty+.38*96, slotx+.09*96, sloty+.38*96, slotx+.09*96, sloty+.225*96, 
+                        slotx+.14*96, sloty+.225*96, slotx+.14*96, sloty+.155*96, slotx+.09*96, sloty+.155*96, 
+                        slotx+.09*96, sloty, slotx, sloty))  
+            elif side == "w" and location == "b": #width and back
+                for i in range(1, 3):  
+                    slotx = i * rec_width/3 + start_x - .045*96
+                    sloty = start_y         
+                    svg.write('<polyline points = "{},{} {},{} {},{} {},{} {},{} {},{}, {},{} {},{} {},{} {},{} {},{} {},{}" ' \
+                        'stroke-width="2" stroke="black" fill="none"/>\n'.format(slotx, sloty, slotx, sloty+.155*96,  
+                        slotx-.05*96, sloty+.155*96, slotx-.05*96, sloty+.225*96, slotx, sloty+.225*96,
+                        slotx, sloty+.38*96, slotx+.09*96, sloty+.38*96, slotx+.09*96, sloty+.225*96, 
+                        slotx+.14*96, sloty+.225*96, slotx+.14*96, sloty+.155*96, slotx+.09*96, sloty+.155*96, 
+                        slotx+.09*96, sloty))               
+            elif side == "l" and location == "r": #length and right                
+                for i in range(1, 3, 1): 
+                    slotx = start_x + rec_width
+                    sloty = i * rec_length/3 + start_y - .045*96         
+                    svg.write('<polyline points = "{},{} {},{} {},{} {},{} {},{} {},{}, {},{} {},{} {},{} {},{} {},{} {},{}" ' \
+                        'stroke-width="2" stroke="black" fill="none" transform="rotate(90,{},{})"/>\n'.format(slotx, sloty, slotx, sloty+.155*96,  
+                        slotx-.05*96, sloty+.155*96, slotx-.05*96, sloty+.225*96, slotx, sloty+.225*96,
+                        slotx, sloty+.38*96, slotx+.09*96, sloty+.38*96, slotx+.09*96, sloty+.225*96, 
+                        slotx+.14*96, sloty+.225*96, slotx+.14*96, sloty+.155*96, slotx+.09*96, sloty+.155*96, 
+                        slotx+.09*96, sloty, slotx, sloty))  
+            elif side == "w" and location == "f": #width and front                
+                for i in range(1, 3, 1):    
+                    slotx = i * rec_width/3 + start_x + .045*96
+                    sloty = start_y + rec_length      
+                    svg.write('<polyline points = "{},{} {},{} {},{} {},{} {},{} {},{}, {},{} {},{} {},{} {},{} {},{} {},{}" ' \
+                        'stroke-width="2" stroke="black" fill="none" transform="rotate(180,{},{})"/>\n'.format(slotx, sloty, slotx, sloty+.155*96,  
+                        slotx-.05*96, sloty+.155*96, slotx-.05*96, sloty+.225*96, slotx, sloty+.225*96,
+                        slotx, sloty+.38*96, slotx+.09*96, sloty+.38*96, slotx+.09*96, sloty+.225*96, 
+                        slotx+.14*96, sloty+.225*96, slotx+.14*96, sloty+.155*96, slotx+.09*96, sloty+.155*96, 
+                        slotx+.09*96, sloty, slotx, sloty))
+            svg.write('<line x1="{}" y1="{}" x2="{}" y2="{}" stroke="black" ' \
+                        'stroke-width="1" />'.format(rec_width/3 + start_x, start_y, rec_width/3 + start_x, start_y + rec_length))
+            svg.write('<line x1="{}" y1="{}" x2="{}" y2="{}" stroke="black" ' \
+                        'stroke-width="1" />'.format(start_x, start_y + rec_length/3, start_x + rec_width, start_y + rec_length/3))
+            svg.write('<line x1="{}" y1="{}" x2="{}" y2="{}" stroke="black" ' \
+                        'stroke-width="1" />'.format(2 * rec_width/3 + start_x, start_y, 2 * rec_width/3 + start_x, start_y + rec_length))
+            svg.write('<line x1="{}" y1="{}" x2="{}" y2="{}" stroke="black" ' \
+                        'stroke-width="1" />'.format(start_x, start_y + 2 * rec_length/3, start_x + rec_width, start_y + 2 * rec_length/3))
+
+
+        # for i in range(1, 3):  
+        #     slotx = i * rec_width/3 + start_x
+        #     sloty = start_y        
+        #     svg.write('<polyline points = "{},{} {},{} {},{} {},{} {},{} {},{}, {},{} {},{} {},{} {},{} {},{} {},{}" ' \
+        #         'stroke-width="2" stroke="black" fill="none"/>\n'.format(slotx, sloty, slotx, sloty+.155*96,  
+        #         slotx-.05*96, sloty+.155*96, slotx-.05*96, sloty+.225*96, slotx, sloty+.225*96,
+        #         slotx, sloty+.38*96, slotx+.09*96, sloty+.38*96, slotx+.09*96, sloty+.225*96, 
+        #         slotx+.14*96, sloty+.225*96, slotx+.14*96, sloty+.155*96, slotx+.09*96, sloty+.155*96, 
+        #         slotx+.09*96, sloty))
+
+
+
+        # #screw holes for base
+        # hole(svg, "w", "f", start_x, start_y, rec_length, rec_width)
+        # hole(svg, "w", "b", start_x, start_y, rec_length, rec_width)
+        # hole(svg, "l", "r", start_x, start_y, rec_length, rec_width)
+        # hole(svg, "l", "l", start_x, start_y, rec_length, rec_width)
+
+        slot(svg, "w", "f", start_x, start_y, rec_length, rec_width)
+        slot(svg, "w", "b", start_x, start_y, rec_length, rec_width)
+        slot(svg, "l", "r", start_x, start_y, rec_length, rec_width)
+        slot(svg, "l", "l", start_x, start_y, rec_length, rec_width)
 
 
 
